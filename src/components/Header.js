b9a2823ce7857a33757c56c156e4dba22eeb6437
Werner Jacobus van Rensburg <22569049@sun.ac.za>
2022-11-05 17:40:41 +0200

    finished header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 
+import './static/styles/header.css';
+
 function HeaderComponent() {
 
     return(
-        <div className="Footer">
-            <Container>
-                <Row>
-                    <Col xs={6} md={4}>
-                        Call Us
+        <div className="header-container">
+            <div className="header-spacer"></div>
+            <Container fluid>
+                <Row className="header-row">
+                    <Col xs={12} md={6} className="contact-details d-flex justify-content-md-start justify-content-center">
+                        <div className="inner">
+                            <b className="emph">CALL US&nbsp;:</b><span className="no-emph">&nbsp;044&nbsp;695&nbsp;0831&nbsp;|</span>
+                            <b className="emph">&nbsp;EMAIL&nbsp;:</b><span className="no-emph">&nbsp;info@testpro.co.za</span>
+                        </div>
                     </Col>
-                    <Col xs={6} md={4}>
-                        Email Us
-                    </Col>
-                    <Col xs={12} md={4}>
-                        Links to other media
+                    <Col xs={12} md={6} className="media-links d-flex justify-content-md-end justify-content-center">
+                        <div className="inner">
+                            <i className="icon fa-brands fa-square-facebook fa-xl"></i>
+                            <i className="icon fa-brands fa-linkedin fa-xl"></i>
+                        </div>
                     </Col>
                 </Row>
             </Container>
+            <div className="header-spacer"></div>
         </div>
     );
 }
