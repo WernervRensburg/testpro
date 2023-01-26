89f9d40e10ebbb3a5b91f8656a0950b2bf67631d
Werner Jacobus van Rensburg <22569049@sun.ac.za>
2023-01-26 13:32:25 +0200

    Started working on Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,13 +2,30 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import { useEffect, useRef, useState } from 'react';
+
+import { Wrapper, Status } from '@googlemaps/react-wrapper';
+
+import './static/styles/contact.css';
+
 function ContactPage() {
     return (
         <>
             <Container fluid>
-                <Row className="d-flex justify-content-center">
-                    <Col xs={12} className="d-flex justify-content-center">
-                        Contact
+                <Row className="d-flex justify-content-center contact-wrapper">
+                    <Col xs={12} sm={4} className="d-flex justify-content-center container-contact">
+                        Image
+                    </Col>
+                    <Col xs={12} sm={4} className="d-flex justify-content-center container-contact">
+                        Details
+                    </Col>
+                    <Col xs={12} sm={4} className="d-flex justify-content-center container-contact">
+                        Map
+                    </Col>
+                </Row>
+                <Row className="d-flex justify-content-center form-wrapper">
+                    <Col xs={12} className="d-flex justify-content-center container-map">
+                        Contact Form
                     </Col>
                 </Row>
             </Container>
